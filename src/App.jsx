00265bc5065b5261e644a905c944902cfb71b176
Zofia Nora <sophienora92@gmail.com>
2025-04-18 15:34:27 +0200

    added components to App Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import Header from './components/Header'
+import Structure from './components/Structure'
+import Content from './components/Content'
+import Interaction from './components/Interactions'
+import States from './components/States'
+import Tech from './components/Tech'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Structure />
+      <Content />
+      <Interaction />
+      <States />
+      <Tech />
+      <Footer />
     </>
   )
 }
