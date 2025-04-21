0430aaf9329b178d8a1118bb0f39d2f068b6327c
Zofia Nora <sophienora92@gmail.com>
2025-04-21 18:41:58 +0200

    added structure component

--- src/components/Structure.jsx
+++ src/components/Structure.jsx
@@ -1,6 +1,27 @@
 function Structure() {
     return(
         <>
+        <section className="structure">
+            <div className="container">
+                <div className="container__structure">
+                    <div className="head">
+                        <h3>Structure & Navigation</h3>
+                    </div>
+                    <div>
+                        <input className="checkbox" type="checkbox" id="checkbox" name="Dobe" />
+                        <p></p>
+                        <svg className="plus" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" color="#384047">
+                            <line class="vert-line" x1="12" y1="5" x2="12" y2="19"></line>
+                            <line x1="5" y1="12" x2="19" y2="12"></line>
+                        </svg>
+                    </div>
+                        
+
+
+                   
+                </div>
+            </div>
+        </section>
         </>
     )
 }
