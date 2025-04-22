d2b297a418818fe55829930ba9d97e87154ae42f
Zofia Nora <sophienora92@gmail.com>
2025-04-22 17:16:36 +0200

    basic strucutre of second headlines and content

--- src/components/Structure.jsx
+++ src/components/Structure.jsx
@@ -7,18 +7,12 @@ function Structure() {
                     <div className="head">
                         <h3>Structure & Navigation</h3>
                     </div>
-                    <div>
+                    <div className="content">
                         <input className="checkbox" type="checkbox" id="checkbox" name="Dobe" />
-                        <p></p>
-                        <svg className="plus" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" color="#384047">
-                            <line class="vert-line" x1="12" y1="5" x2="12" y2="19"></line>
-                            <line x1="5" y1="12" x2="19" y2="12"></line>
-                        </svg>
-                    </div>
+                        <p>Semantic HTML</p>
+                        <button>+</button>
                         
-
-
-                   
+                    </div>  
                 </div>
             </div>
         </section>
