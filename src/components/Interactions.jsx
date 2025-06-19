718061c0a8dadc5221c99583e8ea83ec7edb7831
Zofia Nora <sophienora92@gmail.com>
2025-06-19 16:36:13 +0200

    changed flex direction for headlines

--- src/components/Interactions.jsx
+++ src/components/Interactions.jsx
@@ -5,6 +5,7 @@ function Interactions() {
                 <div className="container__structure">
                     <div className="head">
                         <h2>Forms & Interactions</h2>
+                        <p>This section ensures that users — especially those using screen readers or keyboards — can understand and interact with forms and UI elements effectively.</p>
                     </div>
                 </div>
             </div>
