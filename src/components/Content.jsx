df78ebff37c12eb94f4444065c2bb754fe0ca56c
Zofia Nora <sophienora92@gmail.com>
2025-06-19 15:44:35 +0200

    added file basics for color

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,27 @@
+import Color from './content/Color';
+import ColorUse from './content/ColorUse';
+import Font from './content/Font';
+import Layout from './content/Layout';
+import Text from './content/Text';
+import Video from './content/Video';
+
+
 function Content() {
     return (
         <>
+            <div className="container">
+                    <div className="container__structure">
+                        <div className="head">
+                            <h2>Perceivable Content (Text, Color, Media)</h2>
+                        </div>
+                    </div>
+            </div>
+            <Color />
+            <ColorUse />
+            <Font />
+            <Layout />
+            <Text />
+            <Video />
         </>
     )
 }
