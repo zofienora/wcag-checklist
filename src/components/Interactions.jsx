bb647019d4d47221cc9145a00f208fcd7572c654
Zofia Nora <sophienora92@gmail.com>
2025-06-19 16:34:12 +0200

    added third part for forms and interactions

--- src/components/Interactions.jsx
+++ src/components/Interactions.jsx
@@ -1,6 +1,13 @@
 function Interactions() {
     return(
         <>
+            <div className="container">
+                <div className="container__structure">
+                    <div className="head">
+                        <h2>Forms & Interactions</h2>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
