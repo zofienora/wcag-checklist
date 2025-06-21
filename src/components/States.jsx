aa88e35b426605d359cf7e19301ad8751b1561a0
Zofia Nora <sophienora92@gmail.com>
2025-06-21 11:31:47 +0200

    added next topic states and errors

--- src/components/States.jsx
+++ src/components/States.jsx
@@ -1,6 +1,14 @@
 function States() {
     return (
         <>
+        <div className="container">
+            <div className="container__structure">
+                <div className="head">
+                    <h2>Feedback, States & Errors</h2>
+                    <p>This category ensures that users — especially those using assistive technologies — receive clear, timely feedback when interacting with your site.</p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
