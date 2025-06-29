6a636fc9d85b95ca4e4d9fabc5b8cc2507e368ce
Zofia Nora <sophienora92@gmail.com>
2025-06-29 14:54:11 +0200

    added last tpoic file

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,6 +1,14 @@
 function Tech() {
     return(
         <>
+            <div className="container">
+                <div className="container__structure">
+                    <div className="head">
+                        <h2>Assistive Tech & Responsive Support</h2>
+                        <p>This category ensures your site works smoothly across screen readers, mobile devices, and user settings like zoom and reduced motion.</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
